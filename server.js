bf800f94fdb3cc0d863142e2afd94a3470af2ecd
Your Name <you@example.com>
2026-04-26 17:14:06 +0300

    fixed

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const app = express();
 const PORT = process.env.PORT || 3000;
 const API_KEY = process.env.RENDER_API_KEY; // תגדירי כמשתנה סביבה ב-Render
 
-app.get("/services", async (req, res) => {
+app.get("/", async (req, res) => {
   try {
     const response = await fetch("https://api.render.com/v1/services?limit=20", {
       headers: {
